b84a140360a839b5296656d5e2bc1165f06c8c65
Ayiks <cliffysarp@gmail.com>
2021-12-14 08:36:47 -0800

    no changes

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,3 +1,14 @@
 import 'package:flutter/material.dart';
 
 Color CostomBlue=Color.fromRGBO(37, 43, 103, 1);
+
+Color dateColor(String date) {
+switch(date){
+  case 'Yesterday':
+  return Colors.pinkAccent;
+  case 'Today':
+  return Colors.blueAccent;
+  default:
+  return CostomBlue;
+}
+}
